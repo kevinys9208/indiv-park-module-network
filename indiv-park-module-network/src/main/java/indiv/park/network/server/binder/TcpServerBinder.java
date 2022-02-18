d64e37240dbe097ef158d0b09122d98a23984cb9
zrere <zrere@MyNoteBook>
2022-02-18 22:46:32 +0900

    master

--- indiv-park-module-network/src/main/java/indiv/park/network/server/binder/TcpServerBinder.java
+++ indiv-park-module-network/src/main/java/indiv/park/network/server/binder/TcpServerBinder.java
@@ -39,7 +39,6 @@ public class TcpServerBinder extends ServerBinder {
 					.group(bossGroup, workerGroup)
 					.channel(NioServerSocketChannel.class)
 					.childHandler(new ServerChannelInitializer(serverHandlerList, distinguisher))
-					.option(ChannelOption.TCP_NODELAY, true)
 					.option(ChannelOption.SO_REUSEADDR, true)
 					.option(ChannelOption.SO_BACKLOG, 128)
 					.childOption(ChannelOption.SO_KEEPALIVE, true);
